d046defd0f54830fdb09f16f8d74f5e01831e1d9
nupurvikhankar2 <110446989+nupurvikhankar2@users.noreply.github.com>
2024-07-17 23:35:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 public static void main(String[]args){
-System.out.println("-----------*----------Welcome to Jenkins Training changes made by me-----------*-----------");
+System.out.println("-----------*----------Welcome to Jenkins Training-----------*-----------");
 }
 }
